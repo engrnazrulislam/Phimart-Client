0aaa5f677ed02598b749d1348b9381b05ef7130f
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-11-01 09:45:55 +0600

    update useCart

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -3,11 +3,16 @@ import api_clients from '../services/api_clients';
 
 const useCart = () => {
     // Create new cart
-    const [authToken, setAuthToken] = useState(()=>JSON.parse(localStorage.getItem('authTokens'))?.access);
+    const [authToken, setAuthToken] = useState(()=>JSON.parse(localStorage.getItem("authTokens"))?.access);
     const createCart = async()=>{
         console.log(authToken);
         try {
-            const response = await api_clients.post('/carts/', {}, {headers:{Authorization:`JWT ${authToken}`}});
+            const response = await api_clients.post(
+                '/carts/', 
+                {}, 
+                {headers:{Authorization: `JWT ${authToken}` },
+            }
+        );
             console.log(response.data);
         } catch (error) {
             console.log(error);
